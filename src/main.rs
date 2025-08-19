5ead47968755b0a7a6f015e8ca5be50688fb0847
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-08-19 20:00:34 +0700

    update swap message

--- src/main.rs
+++ src/main.rs
@@ -58,39 +58,10 @@ fn main() {
         .as_ref()
         .and_then(|c| c.success_messages.clone())
         .unwrap_or_else(|| vec![
-            // generic
-            format!("amazing work as always"),
-            format!("you are doing amazing"),
-            // good sweetie
-            format!("that's a good {}", sweetie),
-            format!("who's my good {}", sweetie),
-            format!("that's it, my {}", sweetie),
-            format!("good job, {}", sweetie),
-            format!("good {}", sweetie),
-            // sweet praise
-            format!("you did so well, {}", sweetie),
-            format!("look at my clever girl go~"),
-            format!("you're shining today, {}", sweetie),
-            format!("my {} is unstoppable!", sweetie),
-            // teasing love
-            format!("my gorgeous {} always delivers", sweetie),
-            format!("you're so cute when you focus~"),
-            format!("mm~ look at you showing off~"),
-            format!("you make me proud every time, {}", sweetie),
-            // proud
-            format!("i'm very proud of you"),
-            format!("i'm very proud of you, my {}", sweetie),
-            format!("i'm so proud of you, {}~", sweetie),
-            format!("i knew you could do it"),
-            format!("i knew you could do it, {}~", sweetie),
-            format!("look at you go, {}!", sweetie),
-            // reward
-            format!("*pet your head~*"),
-            format!("*kiss your cheek~*"),
-            format!("you deserve a big kiss for that~"),
-            format!("i think you deserve a special treat for that"),
-            format!("come here, i owe you a cuddle~"),
-            format!("a little reward for my {}!", sweetie),
+            "amazing work as always".to_string(),
+            "you are doing amazing".to_string(),
+            "that's a good {sweetie}".to_string(),
+            "good job, {sweetie}".to_string(),
         ]);
 
     // fail messages
@@ -98,33 +69,8 @@ fn main() {
         .as_ref()
         .and_then(|c| c.fail_messages.clone())
         .unwrap_or_else(|| vec![
-            // trust
-            format!("don't worry, {}. you can do it next time!", sweetie),
-            format!("it's okay, {}. keep trying!", sweetie),
-            format!("i believe in you~"),
-            format!("i believe in you, my {}~", sweetie),
-            format!("i know you'll get there"),
-            format!("i know my little {} can do better", sweetie),
-            format!("you can do better, {}", sweetie),
-            // fallback
-            format!("just know that i still love you~"),
-            format!("i'm always here for you~"),
-            format!("i'm always here for you if you need me"),
-            format!("come here, sit on my lap while we figure this out together"),
-            // consolation
-            format!("don't worry, it'll be alright"),
-            format!("it's okay to make mistakes"),
-            format!("it's okay to make mistakes, my {}", sweetie),
-            format!("i know it's hard, but it will be okay"),
-            format!("even mistakes are part of learning, {}", sweetie),
-            // encouragement
-            format!("never give up, my love"),
-            format!("just a little further, i know you can do it"),
-            format!("i know you'll get there, don't worry about it"),
-            format!("keep pushing, you're stronger than you think, {}", sweetie),
-            // clean up
-            format!("did my {} make a big mess", sweetie),
-            format!("we'll clean this up together, {}", sweetie),
+            "don't worry, {sweetie}. you can do it next time!".to_string(),
+            "it's okay, {sweetie}. keep trying!".to_string(),
         ]);
 
     // Choose message category from exit code
